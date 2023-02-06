44f7ed96c57462782fb0f8e9b446c2c3820b70a4
koobonyeon <bonyeun0605@naver.com>
2023-02-06 19:16:31 +0900

    프론트 컨트롤러로 전환

--- src/main/java/com/bonyeon/helloboot/HellobootApplication.java
+++ src/main/java/com/bonyeon/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -15,25 +16,32 @@ import java.io.IOException;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+    public static void main(String[] args) {
+        ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = webServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+                }
+            }).addMapping("/*");
 
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
-				}
-			}).addMapping("/hello");
+        });
+        webServer.start();
 
-
-		});
-		webServer.start();
-
-	}
+    }
 
 }
