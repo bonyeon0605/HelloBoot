bc49d7c550295dda4ad6963b075aabc158d91a71
koobonyeon <bonyeun0605@naver.com>
2023-02-06 19:26:13 +0900

    Hello 컨트롤러 매핑과 바인딩

--- src/main/java/com/bonyeon/helloboot/HelloController.java
+++ src/main/java/com/bonyeon/helloboot/HelloController.java
@@ -1,13 +1,7 @@
 package com.bonyeon.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-
-    @GetMapping
     public String hello(String name) {
-        return "Hello Spring Boot!" + name;
+        return "Hello Spring Boot! " + name;
     }
 }
