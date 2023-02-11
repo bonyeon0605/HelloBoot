4ffb7ec7de0d9dfc10d43600b83878b62f9fa34e
koobonyeon <bonyeun0605@naver.com>
2023-02-11 20:09:34 +0900

    스프링 컨테이너로 통합

--- src/main/java/com/bonyeon/helloboot/HellobootApplication.java
+++ src/main/java/com/bonyeon/helloboot/HellobootApplication.java
@@ -8,19 +8,21 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = webServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", // GenericWebApplicationContext를 사용해야 dispatcherServlet사용 가능
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class); // 빈 등록
         applicationContext.registerBean(SimpleHelloService.class);  // 인터페이스가 아닌 클래스 타입을 넣어야 한다.
         applicationContext.refresh(); // 초기화 작업
-
-        ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet", // GenericWebApplicationContext를 사용해야 dispatcherServlet사용 가능
-                            new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        webServer.start();
-
     }
-
 }
