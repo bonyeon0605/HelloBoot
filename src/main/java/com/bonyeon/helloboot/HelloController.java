22dcc30e604523824ba5fbdd905772847c6f75ee
koobonyeon <bonyeun0605@naver.com>
2023-02-11 20:29:20 +0900

    @Component 스캔

--- src/main/java/com/bonyeon/helloboot/HelloController.java
+++ src/main/java/com/bonyeon/helloboot/HelloController.java
@@ -2,10 +2,11 @@ package com.bonyeon.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RestController
 @RequestMapping
 public class HelloController {
     private final HelloService helloService;
@@ -15,7 +16,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody // 해당 어노테이션이 없으면 스프링은 view 경로를 찾기 때문에 404 발생
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
