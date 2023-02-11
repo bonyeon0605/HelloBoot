b37faf9b260d6b983fb8898b86dc05a26285e9c0
koobonyeon <bonyeun0605@naver.com>
2023-02-11 20:18:11 +0900

    자바코드 구성 정보 사용

--- src/main/java/com/bonyeon/helloboot/HellobootApplication.java
+++ src/main/java/com/bonyeon/helloboot/HellobootApplication.java
@@ -3,26 +3,39 @@ package com.bonyeon.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
                 ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
                 WebServer webServer = webServerFactory.getWebServer(servletContext -> {
                     servletContext.addServlet("dispatcherServlet", // GenericWebApplicationContext를 사용해야 dispatcherServlet사용 가능
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
+                                new DispatcherServlet(this))
+                            .addMapping("/*");
                 });
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class); // 빈 등록
-        applicationContext.registerBean(SimpleHelloService.class);  // 인터페이스가 아닌 클래스 타입을 넣어야 한다.
+        applicationContext.register(HellobootApplication.class); // 빈 등록
         applicationContext.refresh(); // 초기화 작업
     }
 }
