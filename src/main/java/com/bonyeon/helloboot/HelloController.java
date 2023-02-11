6ec5e62fb2ac29f51d1fc429ff6486a298c78873
koobonyeon <bonyeun0605@naver.com>
2023-02-11 20:05:50 +0900

    어노테이션 매핑 정보 사용

--- src/main/java/com/bonyeon/helloboot/HelloController.java
+++ src/main/java/com/bonyeon/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.bonyeon.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,8 +14,9 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody // 해당 어노테이션이 없으면 스프링은 view 경로를 찾기 때문에 404 발생
     public String hello(String name) {
-
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
