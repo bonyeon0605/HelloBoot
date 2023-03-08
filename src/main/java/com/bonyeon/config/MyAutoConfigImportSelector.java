fcaccb30e8b1c14969ec7c477d0ae234a1d1a9ac
koobonyeon <bonyeun0605@naver.com>
2023-03-08 13:12:38 +0900

    자동 구성 정보 파일 분리

--- src/main/java/com/bonyeon/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/bonyeon/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,27 @@
 package com.bonyeon.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+@RequiredArgsConstructor
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-          "com.bonyeon.config.autoconfig.DispatcherServletConfig",
-          "com.bonyeon.config.autoconfig.TomcatWebServerConfig"
 
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
+
 }
