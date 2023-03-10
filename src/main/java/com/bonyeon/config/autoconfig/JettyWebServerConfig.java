13310fb8cbbbb9bc302038ba37639634fa0bb5d1
koobonyeon <bonyeun0605@naver.com>
2023-03-10 22:54:06 +0900

    스타터와 Jetty 서버 구성

--- src/main/java/com/bonyeon/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/bonyeon/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,16 @@
 package com.bonyeon.config.autoconfig;
 
 import com.bonyeon.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
