b23ce8dbe7377be8eba5f2255751e631fb89203c
koobonyeon <bonyeun0605@naver.com>
2023-03-12 20:49:21 +0900

    @Conditional과 Condition

--- src/main/java/com/bonyeon/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/bonyeon/config/autoconfig/JettyWebServerConfig.java
@@ -3,14 +3,22 @@ package com.bonyeon.config.autoconfig;
 import com.bonyeon.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
