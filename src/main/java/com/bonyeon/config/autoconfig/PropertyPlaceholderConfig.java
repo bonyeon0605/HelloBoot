3276a05a276334ceaf573a27c75d44fe01bccc65
koobonyeon <bonyeun0605@naver.com>
2023-04-12 22:07:02 +0900

    @Value와 PropertySourcesPlaceholderConfigurer

---
+++ src/main/java/com/bonyeon/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package com.bonyeon.config.autoconfig;
+
+import com.bonyeon.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/com/bonyeon/helloboot/HellobootApplication.java
+++ src/main/java/com/bonyeon/helloboot/HellobootApplication.java
@@ -1,10 +1,7 @@
 package com.bonyeon.helloboot;
 
 import com.bonyeon.config.MySpringBootApplication;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class HellobootApplication {
