5c71487737f5dc1cd87432daf8ef5545d306b6ff
koobonyeon <bonyeun0605@naver.com>
2023-04-12 21:58:03 +0900

    자동 구성에 Enviroment 프로퍼티 적용

--- src/main/java/com/bonyeon/helloboot/HellobootApplication.java
+++ src/main/java/com/bonyeon/helloboot/HellobootApplication.java
@@ -1,7 +1,10 @@
 package com.bonyeon.helloboot;
 
 import com.bonyeon.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class HellobootApplication {
@@ -9,4 +12,5 @@ public class HellobootApplication {
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class ,args);
     }
+
 }
